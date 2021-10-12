c8accf6eec43bc12eab4385901dd528003379f25
Razzacktiger <harrazzack@gmail.com>
2021-10-12 16:23:17 -0700

    removed test funciton from calculator

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 import math
 def calculator(number1, number2, operator):
+    """Performs """
     if number1.isdigit() and number2.isdigit():
         number1 = int(number1)
         number2 = int(number1)
@@ -21,11 +22,13 @@ def calculator(number1, number2, operator):
         result = number1 % number2
     return result 
 def parse_input():
+    """
+    takes in user inout and parses it into arguments which are passed into the calculator 
+    """
     request = input("Enter Equation: ")
     arguments = request.split()
     print(calculator(arguments[0], arguments[2], arguments[1]))
 
-parse_input()
 
 
 
